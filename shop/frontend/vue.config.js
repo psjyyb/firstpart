77588774523d840c6a82daa511c697d2f2bb7fca
seohwaj <yeohhye@naver.com>
2024-06-25 17:56:04 +0900

    vue.config.js 수정

--- shop/frontend/vue.config.js
+++ shop/frontend/vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
-const target = "http://localhost:3000";
 
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -8,7 +7,7 @@ module.exports = defineConfig({
     port: 8080,
     proxy: {
       "^/api": {
-        target,
+        target: "http://localhost:3000",
         changeOrigin: true,
         pathRewrite: {'^/api': ''}
       },
