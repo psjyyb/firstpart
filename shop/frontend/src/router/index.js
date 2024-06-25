c64acfc05556e5bda177ba751fc2619b3f8fc372
psjyyb <ass6728@naver.com>
2024-06-25 17:26:57 +0900

    박성준

--- shop/frontend/src/router/index.js
+++ shop/frontend/src/router/index.js
@@ -1,12 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import MypageView from '../mypage/MypageFirst.vue'
-
+import MypageSideVar from '../components/SideVar.vue'
+import MypageOrderList from '../mypage/MypageOrderList.vue'
+import MypageCancelList from '../mypage/MypageCancelList.vue'
 
 const routes = [
   {
-    path: '/mypageView',
-    name: 'mypageView',
+    path: '/mypageCancelList',
+    name: 'mypageCancelList',
+    component: MypageCancelList
+  },
+  {
+    path: '/mypageOrderList',
+    name: 'mypageOrderList',
+    component: MypageOrderList
+  },
+  {
+    path: '/mypageSideVar',
+    name: 'mypageSideVar',
+    component: MypageSideVar
+  },
+  {
+    path: '/mypage',
+    name: 'mypage',
     component: MypageView
   },
   {
