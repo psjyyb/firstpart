15a5283714820b63603555d628ae9e7b804260ea
podotang <syj6180@naver.com>
2024-06-25 10:22:14 +0900

    .

--- shop/frontend/src/mypage/MypageFirst.vue
+++ shop/frontend/src/mypage/MypageFirst.vue
@@ -6,11 +6,9 @@
     <!-- <Footer /> -->
 </template>
 <script>
-import Header from "@/components/Header.vue"
-import Footer from "@/components/Footer.vue"
+
 
     export default{
-        components: { Header, Footer },
     data(){
      return {}; 
     },
