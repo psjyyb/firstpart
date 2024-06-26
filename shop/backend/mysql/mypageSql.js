2d1db08d79d7e4c107e95acce355417f2adcaa3f
psjyyb <ass6728@naver.com>
2024-06-26 14:51:38 +0900

    sad

--- shop/backend/mysql/mypageSql.js
+++ shop/backend/mysql/mypageSql.js
@@ -28,20 +28,56 @@ module.exports = {
                         group by o.order_date,o.order_status,p.product_name 
                         order by order_date`,
 
-    mypageOrderList : `SELECT o.order_no,o.user_id,o.order_date,o.order_status,IFNULL(COUNT(d.product_no), 0) AS product_count,GROUP_CONCAT(p.product_name SEPARATOR ', ') AS product_names,GROUP_CONCAT(p.product_img SEPARATOR ', ') AS product_images FROM orders o LEFT JOIN order_detail d ON o.order_no = d.order_no LEFT JOIN product p ON p.product_no = d.product_no WHERE o.user_id = ? GROUP BY o.order_no order by o.order_no desc limit ?,?`,
-    mypageOrderListCount : `select count(*) as cnt from orders where user_id= ? `,
-    mypageCancelList:`SELECT c.cancel_no,c.cancel_date,c.cancel_state,d.product_no,o.pay_price FROM cancel c JOIN order_detail d ON c.order_no = d.order_no JOIN orders o ON o.order_no = d.order_no WHERE o.user_id = ? order by o.order_no desc limit ?,?`,
-    mypageCancelCount:`SELECT COUNT(*) as cnt FROM cancel WHERE order_no IN (SELECT order_no FROM orders WHERE user_id = ? )`,
+    mypageOrderList : `SELECT o.order_no,o.user_id,o.order_date,o.order_status,IFNULL(COUNT(d.product_no), 0) AS product_count
+                      ,GROUP_CONCAT(p.product_name SEPARATOR ', ') AS product_names,GROUP_CONCAT(p.product_img SEPARATOR ', ') AS product_images 
+                      FROM orders o LEFT JOIN order_detail d ON o.order_no = d.order_no 
+                      LEFT JOIN product p ON p.product_no = d.product_no WHERE o.user_id = ?
+                      GROUP BY o.order_no order by o.order_no desc limit ?,?`,
+    mypageOrderListCount : `select count(*) as cnt 
+                            from orders where user_id= ? `,
+    mypageCancelList:`SELECT c.cancel_no,c.cancel_date,c.cancel_state,d.product_no,o.pay_price 
+                        FROM cancel c JOIN order_detail d ON c.order_no = d.order_no JOIN orders o 
+                        ON o.order_no = d.order_no WHERE o.user_id = ? order by o.order_no desc limit ?,?`,
+    mypageCancelCount:`SELECT COUNT(*) as cnt
+                         FROM cancel 
+                         WHERE order_no IN (SELECT order_no FROM orders WHERE user_id = ? )`,
 
-    mypageWishList:`select wish_no,product_name,product_price,product_img from wish w join product p on w.product_no=p.product_no where user_id = ? order by p.product_no desc limit ?,?`,
-    mypageWishListCount:`select count(*) as cnt from wish where user_id = ?`,
+    mypageWishList:`select wish_no,product_name,product_price,product_img 
+                    from wish w join product p on w.product_no=p.product_no 
+                    where user_id = ? order by p.product_no desc limit ?,?`,
+    mypageWishListCount:`select count(*) as cnt 
+                        from wish where user_id = ?`,
 
-    mypageQnAList:`select * from qna where user_id = ? order by qna_date desc limit ?,?`,
-    mypageQnAListCount:`select count(*) as cnt from qna where user_id=?`,
+    mypageQnAList:`select * from qna where user_id = ? 
+                    order by qna_date desc limit ?,?`,
+    mypageQnAListCount:`select count(*) as cnt 
+                        from qna where user_id=?`,
     //작성가능리뷰
-    mypageYesReviewList: `SELECT d.order_no, d.product_no, p.product_name, p.product_img, p.product_price,order_date FROM orders o JOIN order_detail d ON o.order_no = d.order_no JOIN product p ON p.product_no = d.product_no LEFT JOIN review r ON r.order_no = d.order_no AND r.product_no = d.product_no WHERE o.user_id = ? AND r.review_no IS NULL order by review_no desc limit ?,?`,
-    mypageYesReviewListCount:`SELECT COUNT(*) as ycnt FROM orders o JOIN order_detail d ON o.order_no = d.order_no JOIN product p ON p.product_no = d.product_no LEFT JOIN review r ON r.order_no = d.order_no AND r.product_no = d.product_no WHERE o.user_id = ? AND r.review_no IS NULL`,
+    mypageYesReviewList: `SELECT d.order_no, d.product_no, p.product_name,
+                         p.product_img, p.product_price,order_date 
+                         FROM orders o JOIN order_detail d ON o.order_no = d.order_no 
+                         JOIN product p ON p.product_no = d.product_no 
+                         LEFT JOIN review r ON r.order_no = d.order_no 
+                         AND r.product_no = d.product_no WHERE o.user_id = ?
+                         AND r.review_no IS NULL order by review_no desc limit ?,?`,
+    mypageYesReviewListCount:`SELECT COUNT(*) as ycnt 
+                            FROM orders o JOIN order_detail d ON o.order_no = d.order_no 
+                            JOIN product p ON p.product_no = d.product_no
+                            LEFT JOIN review r ON r.order_no = d.order_no .
+                            AND r.product_no = d.product_no 
+                            WHERE o.user_id = ? AND r.review_no IS NULL`,
     //작성한 리뷰
-    mypageNoReviewList: `SELECT d.order_no, d.product_no, p.product_name, p.product_img, p.product_price,review_date FROM orders o JOIN order_detail d ON o.order_no = d.order_no JOIN product p ON p.product_no = d.product_no JOIN review r ON r.order_no = d.order_no AND r.product_no = d.product_no WHERE o.user_id = ? order by review_no desc limit ?,?`,
-    mypageNoReviewListCount:`SELECT COUNT(*) as ncnt FROM orders o JOIN order_detail d ON o.order_no = d.order_no JOIN product p ON p.product_no = d.product_no JOIN review r ON r.order_no = d.order_no AND r.product_no = d.product_no WHERE o.user_id = ?`,
+    mypageNoReviewList: `SELECT d.order_no, d.product_no, p.product_name, p.product_img, p.product_price,review_date
+                        FROM orders o JOIN order_detail d ON o.order_no = d.order_no 
+                        JOIN product p ON p.product_no = d.product_no 
+                        JOIN review r ON r.order_no = d.order_no
+                        AND r.product_no = d.product_no
+                        WHERE o.user_id = ? 
+                        order by review_no desc limit ?,?`,
+    mypageNoReviewListCount:`SELECT COUNT(*) as ncnt 
+                            FROM orders o JOIN order_detail d ON o.order_no = d.order_no 
+                            JOIN product p ON p.product_no = d.product_no 
+                            JOIN review r ON r.order_no = d.order_no 
+                            AND r.product_no = d.product_no 
+                            WHERE o.user_id = ?`,
 }
