08c75cd41a78e1593bea26785281e52ee0124dab
podotang <131725389+podotang@users.noreply.github.com>
2024-06-27 08:31:17 +0900

    Update mixin.js

--- shop/frontend/src/mixin.js
+++ shop/frontend/src/mixin.js
@@ -17,7 +17,7 @@ export default {
         },
         
         $currencyFormat(value, format = "#,###") {
-            // 가격,등 세자리마다 ,을 찍어주는 메소드
+            
             if (value == 0 || value == null) return 0;
       
             var currency = format.substring(0, 1);
