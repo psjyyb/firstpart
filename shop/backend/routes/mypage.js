52d18c72d4ec4991889bb271f33826e543d1fc88
ass6728 <ass6728@naver.com>
2024-07-08 21:59:31 +0900

    dfsfs

--- shop/backend/routes/mypage.js
+++ shop/backend/routes/mypage.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const query = require('../mysql/index.js');
 const multer = require('multer');
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) { //파일이 저장 될 위치 지정
+    destination: function (req, file, cb) {
       cb(null, 'C:/Users/admin/git/firstpart/shop/backend/upload/parkseongjun'); 
     },
     filename: function (req, file, cb) {
-      const originalname = Buffer.from(file.originalname, 'latin1').toString('utf8'); // 파일 utf-8로 변환
-      //const temName=(file.originalname, 'latin1').toString('utf8'); // 파일 utf-8로 변환
+      const originalname = Buffer.from(file.originalname, 'latin1').toString('utf8');
+      //const temName=(file.originalname, 'latin1').toString('utf8')
       cb(null, Date.now() + '-' + originalname);
     }
   });
